ec6e0a830f283160bc99c3127b5af088974737a6
Keelan Daye <keelan.daye@gmail.com>
2022-07-22 02:31:01 +0100

    New button variants

--- stories/Button.stories.jsx
+++ stories/Button.stories.jsx
@@ -6,6 +6,10 @@ export default {
     title: 'Button',
     component: 'Button',
     argTypes: {
+        variant: {
+            options: ['primary', 'secondary', 'success', 'warning', 'danger'],
+            control: 'select',
+        },
         backgroundColor: { control: 'color' },
         onClick: { action: 'clicked' },
     },
@@ -15,12 +19,30 @@ const Template = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-    primary: true,
     label: 'Button',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
+    variant: 'secondary',
+    label: 'Button',
+};
+
+export const Success = Template.bind({});
+Success.args = {
+    variant: 'success',
+    label: 'Button',
+};
+
+export const Warning = Template.bind({});
+Warning.args = {
+    variant: 'warning',
+    label: 'Button',
+};
+
+export const Danger = Template.bind({});
+Danger.args = {
+    variant: 'danger',
     label: 'Button',
 };
 
@@ -34,4 +56,10 @@ export const Small = Template.bind({});
 Small.args = {
     size: 'small',
     label: 'Button',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+    disabled: true,
+    label: 'Disabled button',
 };
